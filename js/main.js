3465ba3c99d3bfd7dff67323d7d76f979ae349e0
Alejandro Chavazza <2787257+achavazza@users.noreply.github.com>
2021-03-27 13:26:11 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,99 @@
+const app = Vue.createApp({
+    data() {
+        return{
+            title: "Calculadora Vue",
+            fields: [
+                {
+                    n1: "",
+                    n2: "",
+                    res: 0,
+                    mbp: 0,
+                    ptom: 0.00,
+                    mtop: 0.00,
+                },
+                {
+                    n1: "",
+                    n2: "",
+                    res: 0,
+                    mbp: 0,
+                    ptom: 0.00,
+                    mtop: 0.00,
+                }
+            ]
+        };
+    },
+    computed: {
+        result(){
+            let fieldList = this.fields;
+            let val = 0;
+               
+            fieldList.forEach(field => {                
+                
+                let val = field.n2 / field.n1;
+                let val2 = field.n1 / field.n2;
+
+                if(isNaN(val)){
+                    field.res = "--";
+                    field.mtop = "--";
+                }else{
+                    if (val === Infinity || val === -Infinity){
+                        field.res = "Error"
+                    } else {
+                        field.res = val;
+                        field.mtop = val.toFixed(2) + '$';
+                    }
+                }
+                
+                if(isNaN(val2)){
+                    field.mbp = "--";
+                    field.ptom = "--";
+                }else{
+                    if (val2 === Infinity || val2 === -Infinity){
+                        field.mbp = "Error"
+                    } else {
+                        field.mbp = val2;    
+                        field.ptom = val2.toFixed(2) + 'm';
+                    }
+                }
+            });
+            var fields = this.fields
+        },
+        log(){
+            return this.fields;
+        },
+        highestEl(){
+            if (this.fields.length == 0 ){
+                return 
+            } 
+            selectedFieldHigh = this.fields.reduce((a,b) => Number(a.res) > Number(b.res) ? a : b);
+            return selectedFieldHigh
+        },
+        lowestEl(){
+            if (this.fields.length == 0 ){
+                return 
+            } 
+            selectedFieldLow = this.fields.reduce((a,b) => Number(a.res) < Number(b.res) ? a : b);
+
+            return selectedFieldLow;
+        }
+    },
+    methods:{
+        addField(){
+            this.fields.push({
+                    n1: "",
+                    n2: "",
+                    res: 0,
+                    mbp: 0,
+                    ptom: 0.00,
+                    mtop: 0.00,
+                });
+        },
+        removeField(index){
+            //this.$delete(this.fields, index)
+            this.fields.splice(index, 1);
+            if(this.fields.length <= 1){
+                this.addField();
+            }
+        },
+    }
+})
---
+++ sw.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = "v1_cache_calculadora_PH";
+const urlsToCache = [
+    "./",
+    "./?umt_source=web_app_manifest",
+    "./img/favicon.png",
+    "./img/icon-32.png",
+    "./img/icon-64.png",
+    "./img/icon-128.png",
+    "./img/icon-192.png",
+    "./img/icon-256.png",
+    "./img/icon-512.png",
+    "./img/icon-1024.png",
+    "./img/maskable.png",
+    "https://unpkg.com/vue@next",
+    "./js/main.js",
+    "./js/mountApp.js",
+    "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap",
+    "https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css",
+    "https://fonts.googleapis.com/icon?family=Material+Icons",
+    "./css/style.css",
+    "./manifest.json",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+      cache
+        .addAll(urlsToCache)
+        .then(() => self.skipWaiting())
+        .catch((err) => console.log(err))
+    )
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        return res;
+      }
+      return fetch(e.request);
+    })
+  );
+});
